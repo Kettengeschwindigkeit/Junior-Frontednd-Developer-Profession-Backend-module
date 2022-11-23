71be74ad568fe998541a81419afbac64cb3431eb
Kettengeschwindigkeit <666damned999@mail.ru>
2022-11-23 12:19:05 +0300

    2. Base. Reading from file

--- notes.controller.js
+++ notes.controller.js
@@ -1,9 +1,17 @@
 const fs = require('fs/promises')
+const path = require('path')
 
-const notes = []
+const notesPath = path.join(__dirname, 'db.json')
+console.log(notesPath)
 
 async function addNote(title) {
-    const notes = require('./db.json')
+    // const notes = require('./db.json')
+
+    // const buffer = await fs.readFile(notesPath)
+    // const notes = Buffer.from(buffer).toString('utf-8')
+
+    const notes = await getNotes()
+
     const note = {
         title,
         id: Date.now().toString()
@@ -11,11 +19,14 @@ async function addNote(title) {
 
     notes.push(note)
 
-    await fs.writeFile('./db.json', JSON.stringify(notes))
+    await fs.writeFile(notesPath, JSON.stringify(notes))
 }
 
-function getNotes() {
-    return require('./db.json')
+addNote('Test!')
+
+async function getNotes() {
+    const notes = await fs.readFile(notesPath, { encoding: 'utf-8' })
+    return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : []
 }
 
 module.exports = {
