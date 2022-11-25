f568d7f6e1ba4429fc3a9ca3589fd7f0ea33de9d
Kettengeschwindigkeit <666damned999@mail.ru>
2022-11-25 14:15:08 +0300

    4. Web-server. Homework

--- public/app.js
+++ public/app.js
@@ -7,9 +7,22 @@ document.addEventListener('click', event => {
         remove(id).then(() => {
             event.target.closest('li').remove()
         })
+    } else if (event.target.dataset.type === 'update') {
+        const id = event.target.dataset.id
+        const title = prompt('Введите новое название')
+
+        if (title !== null) {
+            update(id, title).then(() => {
+                event.target.closest('li').firstElementChild.innerText = title
+            })
+        }
     }
 })
 
 async function remove(id) {
     await fetch(`/${id}`, { method: 'DELETE' })
+}
+
+async function update(id, title) {
+    await fetch(`/${id}`, { method: 'PUT', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ title }) })
 }
